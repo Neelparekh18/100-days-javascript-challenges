01f4f2e8205fe624f5e1656717c651ef13ded951
Neel Parekh <nparekh88888@gmail.com>
2024-04-22 17:07:05 +0530

    Initial commit

---
+++ 01_day.js
@@ -0,0 +1,19 @@
+// Date: 28/03/24
+
+// Question: Longest word in a String.  
+
+let str = "watch virat kohli batting at chinnaswami stadium";
+
+const findLongestWord = (str) => {
+  let longestWord = "";
+  let strToArr = str.split(" ");
+
+  for (let i = 0; i < strToArr.length; i++) {
+    if (strToArr[i].length > longestWord.length) {
+      longestWord = strToArr[i];
+    }
+  }
+  console.log(longestWord);
+  
+};
+findLongestWord(str);
