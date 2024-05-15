67a7126789352e8e373ff29ae016736327b5016d
Neel Parekh <nparekh88888@gmail.com>
2024-05-15 15:59:38 +0530

    Initial commit

---
+++ 20_day.js
@@ -0,0 +1,20 @@
+//Date: 10/05/24
+
+// Question: "Initial Substring Detector: String Start Check"
+
+// Task:
+// Write a function to reverse a string without using in-built methods or libraries. The function should take a string as an input and return the reversed string.
+
+const str = "Neel";
+
+const reverseStr = (str) => {
+  const arr = [];
+  const strToArr = str.split("");
+
+  for (let i = strToArr.length - 1; i >= 0; i--) {
+    arr.push(strToArr[i]);
+  }
+  return arr.join("");
+};
+const result = reverseStr(str);
+console.log(result);
