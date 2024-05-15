aa329252bcd14fde02e9320e9cbcde518b3d0a13
Neel Parekh <nparekh88888@gmail.com>
2024-05-15 16:41:25 +0530

    Resolve mistakes

--- 21_day.js
+++ 21_day.js
@@ -7,13 +7,11 @@
 
 // The means is a the average of a set of numbers, or the numeric value that represents the center of a collection of numbers.
 
-const arr = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 
-
-const calculateMean = (arr)=>{
-    const len = arr.length;
-    const meanIndex = Math.floor(len/2)
-    return arr[meanIndex]
-}
+const calculateMean = (arr) => {
+  const res = arr.reduce((acc, curr) => acc + curr, 0);
+  return res / arr.length;
+};
 const result = calculateMean(arr);
-console.log(result)
+console.log(result);
